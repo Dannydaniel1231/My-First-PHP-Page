04a018b3e84da71bce5ca9c0dbb5e377457b705b
Dannydaniel1231 <ndaniel4040@gmail.com>
2022-09-14 11:56:18 +0200

    initial commit on index.php push to alpha

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP Page</title>
+</head>
+<body>
+username:<input type="text"><br><br>
+password:<input type="password"><br><br>
+Email:<input type="text"><br><br>
+Tel:<input type="tel"><br><br>
+Gender:<input type="radio" value="male" name="gender"><br><br>
+<input type="radio" value="female" name="gender"><br><br>
+Age:<input type="text"><br><br>
+<input type="submit" value="submit">
+ 
+</body>
+</html>
